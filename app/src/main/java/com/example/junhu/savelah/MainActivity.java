151554a6224671310ef624264d5d06919eb936ae
Mick <micklwl@hotmail.com>
2018-06-03 19:03:48 +0800

    Implement register page with check for existing email

--- app/src/main/java/com/example/junhu/savelah/MainActivity.java
+++ app/src/main/java/com/example/junhu/savelah/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.junhu.savelah;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
@@ -34,7 +33,15 @@ public class MainActivity extends AppCompatActivity {
         mAuth.addAuthStateListener(mAuthListener);
     }
 
-    public void onButtonClick(View v){
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.create_account_button:
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+        }
 
     }
+
+
 }
