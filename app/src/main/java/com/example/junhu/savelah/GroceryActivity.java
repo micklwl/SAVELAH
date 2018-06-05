d61e8b2c31905ba13d11f8a45678c534011c7b23
DESKTOP-junhui96 <E0203169@u.nus.edu>
2018-06-05 16:46:42 +0800

    Updating of the grocery activity to include menu for delete option.

--- app/src/main/java/com/example/junhu/savelah/GroceryActivity.java
+++ app/src/main/java/com/example/junhu/savelah/GroceryActivity.java
@@ -2,8 +2,11 @@ package com.example.junhu.savelah;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -11,6 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class GroceryActivity extends AppCompatActivity {
     private EditText toAdd;
+    private ListView groceryList;
 
 
     @Override
@@ -19,13 +23,23 @@ public class GroceryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grocery);
         // Initalise widgets
         toAdd = findViewById(R.id.addText);
+        groceryList = findViewById(R.id.groceryList);
+        registerForContextMenu(groceryList);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.grocery, menu);
     }
 
     private void addGrocery() {
         String str = toAdd.getText().toString().trim();
         if (!str.isEmpty()) {
             DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("Grocery");
-            mDatabase.setValue(str);
+            String listID = mDatabase.push().getKey();
+            mDatabase.child(listID).setValue(str);
         } else {
             Toast.makeText(this,"Please enter grocery name", Toast.LENGTH_SHORT).show();
         }
