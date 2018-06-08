d1cca831849914d5302788ddf8b2a38639978701
Mick <micklwl@hotmail.com>
2018-06-09 02:24:28 +0800

    Adding the bottom navigation bar in layout

--- app/src/main/java/com/example/junhu/savelah/ProfileActivity.java
+++ app/src/main/java/com/example/junhu/savelah/ProfileActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.design.widget.BottomNavigationView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
---
+++ app/src/main/java/com/example/junhu/savelah/RecipeActivity.java
@@ -0,0 +1,47 @@
+package com.example.junhu.savelah;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_grocery:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_recipe:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_calendar:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_profile:
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
