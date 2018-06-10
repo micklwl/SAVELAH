e3897f14738a30b0c0b168e5a6b988dadb9ec080
Mick <micklwl@hotmail.com>
2018-06-10 23:09:26 +0800

    auto add when sign up

--- app/src/main/java/com/example/junhu/savelah/SignUpActivity.java
+++ app/src/main/java/com/example/junhu/savelah/SignUpActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,14 +25,14 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     EditText editTextEmail;
     EditText editTextPassword;
     private FirebaseAuth mAuth;
-    private DatabaseReference mDatabase;
+
     ProgressBar progressBar;
     private static final String TAG = SignUpActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         setContentView(R.layout.activity_sign_up);
         editTextEmail = findViewById(R.id.field_email);
         editTextPassword = findViewById(R.id.field_password);
@@ -42,50 +43,49 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
-    private void registerUser(){
-       final String email = editTextEmail.getText().toString().trim();
+    private void registerUser() {
+        final String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
-        if (email.isEmpty()){
+        if (email.isEmpty()) {
             editTextEmail.setError("Email is required");
             editTextEmail.requestFocus();
             return;
         }
 
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             editTextEmail.setError("Please Enter a Valid Email");
             editTextEmail.requestFocus();
             return;
         }
 
-        if (password.isEmpty()){
+        if (password.isEmpty()) {
             editTextPassword.setError("Password is required");
             editTextPassword.requestFocus();
             return;
         }
         // min length for firebase pw is 6
-        if (password.length() <6){
+        if (password.length() < 6) {
             editTextPassword.setError("Minimum length of password should be 6.");
             editTextPassword.requestFocus();
             return;
         }
+
         progressBar.setVisibility(View.VISIBLE);
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 progressBar.setVisibility(View.GONE);
-                if (task.isSuccessful()){
-                    Customer c = new Customer();
-                    c.setEmail(email);
-                    mDatabase.child(email).setValue(c);
-//                    mDatabase.push().setValue(c);
+                if (task.isSuccessful()) {
+                    FirebaseUser fUser = mAuth.getCurrentUser();
+                    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+                    mDatabase.child("Users:").child(fUser.getUid()).child("Email").setValue(email);
                     Toast.makeText(getApplicationContext(), "User Registered Successful", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
                     Log.w(TAG, "signInWithCredential", task.getException());
-                    if(task.getException() instanceof FirebaseAuthUserCollisionException) {
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
                         Toast.makeText(getApplicationContext(), "User with Email id already exists", Toast.LENGTH_SHORT).show();
-                    }
-                    else Toast.makeText(getApplicationContext(), "Some Error Occurred", Toast.LENGTH_SHORT).show();
+                    } else
+                        Toast.makeText(getApplicationContext(), "Some Error Occurred", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -93,8 +93,8 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     @Override
-    public void onClick(View view){
-        switch(view.getId()){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.register_button:
                 registerUser();
                 break;
@@ -104,4 +104,5 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
                 break;
         }
     }
+
 }
