04fc685bc80f1dadebc56bb85a494eae458b110c
DESKTOP-junhui96 <E0203169@u.nus.edu>
2018-06-17 16:01:38 +0800

    Corrected SharedList Activity

--- app/src/main/java/com/example/junhu/savelah/SharedListActivity.java
+++ app/src/main/java/com/example/junhu/savelah/SharedListActivity.java
@@ -15,6 +15,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.junhu.savelah.dataObjects.DatePickerFragment;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,6 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class SharedListActivity extends AppCompatActivity {
     private EditText toAdd;
@@ -30,13 +35,14 @@ public class SharedListActivity extends AppCompatActivity {
     private ArrayList<String> list = new ArrayList<>();
     private DatabaseReference initDatabase;
     private DatabaseReference mDatabase;
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shared_list);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(GroceryActivity.EXTRA_MESSAGE);
+        message = intent.getStringExtra(GroceryActivity.EXTRA_MESSAGE);
         initDatabase = FirebaseDatabase.getInstance().getReference("Users");
         mDatabase = initDatabase.child(message);
         toAdd = findViewById(R.id.addText);
@@ -47,18 +53,27 @@ public class SharedListActivity extends AppCompatActivity {
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Customer c = dataSnapshot.getValue(Customer.class);
-                list.clear();
-                list.addAll(new ArrayList<String>(c.getRecipe().keySet()));
-                Log.d("hello", "onDataChange: " + list);
-                adapter.notifyDataSetChanged();
+            Customer c = dataSnapshot.getValue(Customer.class);
+            list.clear();
+            HashMap<String, Ingredient> map = c.getRecipe();
+            ArrayList<String> temp = new ArrayList<>();
+            for (Map.Entry<String, Ingredient> entry: map.entrySet()) {
+                String key = entry.getKey();
+                Ingredient value = entry.getValue();
+                temp.add(key +  " " + value.getAmount());
             }
+            list.addAll(temp);
+            // list.addAll(new ArrayList<String>(c.getRecipe().keySet()));
+            Log.d("hello", "onDataChange: " + list);
+            adapter.notifyDataSetChanged();
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
-        });
+        }
+    });
+
         BottomNavigationViewEx bottombar = (BottomNavigationViewEx) findViewById(R.id.navigation);
         bottombar.enableAnimation(false);
         bottombar.enableShiftingMode(false);
@@ -68,6 +83,7 @@ public class SharedListActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()){
                 case R.id.navigation_grocery:
+                    startActivity(new Intent (SharedListActivity.this, GroceryActivity.class));
                     break;
                 case R.id.navigation_recipe:
                     startActivity(new Intent(SharedListActivity.this, RecipeActivity.class)) ;
@@ -92,8 +108,14 @@ public class SharedListActivity extends AppCompatActivity {
     }
 
     private void deleteGrocery(int key) {
-        String item = list.get(key);
+        String item = findItem(key);
         mDatabase.child("recipe").child(item).removeValue();
+    }
+
+    public String findItem(int position) {
+        String item = list.get(position);
+        item = item.substring(0, item.lastIndexOf(" "));
+        return item;
     }
 
     @Override
@@ -104,6 +126,15 @@ public class SharedListActivity extends AppCompatActivity {
                 int position = info.position;
                 deleteGrocery(position);
                 return true;
+            case R.id.updateDate :
+                int p = info.position;
+                DatePickerFragment newFragment = new DatePickerFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString("item", findItem(p));
+                bundle.putString("User", message);
+                newFragment.setArguments(bundle);
+                //   setDate(newFragment.getDate(), p);
+                newFragment.show(getFragmentManager(), "datePicker");
             default:
                 return super.onContextItemSelected(item);
         }
